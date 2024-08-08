f65aa7ed9e4b72092e0fc22c29cfe372b56f1597
farmist <farmist@163.com>
2024-08-08 15:56:01 +0800

    update for test

--- config/pc/env.js
+++ config/pc/env.js
@@ -4,16 +4,16 @@
  */
 const envs = {
   production: {
-    DOMAIN: 'http://h2o-llm.elenet.me',
+    DOMAIN: 'http://122.51.140.189',
     BASE_URL: './', // 路由基础路径
-    BASE_API: 'http://h2o-llm.elenet.me/api',
+    BASE_API: 'http://122.51.140.189/api',
     SYSTEM_API: '/api',
     _ISGZIP: true, // 是否开启gzip压缩
     _ISCDN: true, // 是否开启cdn加速
   },
 }
 const env = Object.assign(
-  { SYSTEM_NAME_ALL: 'LinkWeChat -- 全链路私域数字化营销增长解决方案' },
+  { SYSTEM_NAME_ALL: '企微SCRM' },
   envs[process.env.VUE_APP_ENV || process.env.NODE_ENV],
 )
 
